ae4a0bec7f8f3647d55467de3ac459936fa1e175
miirchv <98304456+miirchv@users.noreply.github.com>
2022-07-31 14:43:40 +0400

    Add files via upload

--- PP_1_1_3-4_JDBC_Hibernate-master (1)/PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master (1)/PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,50 +13,58 @@ import java.util.List;
 public class UserDaoJDBCImpl implements UserDao {
 
 
-    Util newU = new Util();
-    Connection connection = newU.getConnection();
+    private Util newU = new Util();
+    private Connection connection = newU.getConnection();
 
 
 
-    public UserDaoJDBCImpl() throws SQLException {
+    public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
         try {
             connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS users1" + "(id INTEGER not NULL AUTO_INCREMENT, " + " name VARCHAR(40), " + " lastName VARCHAR(40), " + " age INTEGER, " + " PRIMARY KEY ( id ))");
         } catch (SQLException e) {
 
+        } finally {
+            connection.close();
         }
 
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException {
         try {
             connection.createStatement().executeUpdate("DROP TABLE IF EXISTS users1");
         } catch (Exception e) {
 
+        } finally {
+            connection.close();
         }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         try {
             connection.prepareStatement("insert into users1 (name, lastName, age) values ( '" + name + "', '" + lastName + "', '" + age + "')").executeUpdate();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
         } catch (Exception e) {
 
+        } finally {
+            connection.close();
         }
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
         try {
             connection.prepareStatement("delete from users1 where id = " + id).executeUpdate();
-        } catch (SQLException e) {
+        } catch (Exception e) {
 
+        } finally {
+            connection.close();
         }
     }
 
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
         List<User> list = new ArrayList<>();
 
         try {
@@ -74,16 +82,20 @@ public class UserDaoJDBCImpl implements UserDao {
 
         } catch (Exception e) {
 
+        } finally {
+            connection.close();
         }
 
         return list;
     }
 
-    public void cleanUsersTable () {
+    public void cleanUsersTable () throws SQLException {
         try {
             connection.prepareStatement("delete from users1").executeUpdate();
         } catch (Exception e) {
 
+        } finally {
+            connection.close();
         }
     }
 }
