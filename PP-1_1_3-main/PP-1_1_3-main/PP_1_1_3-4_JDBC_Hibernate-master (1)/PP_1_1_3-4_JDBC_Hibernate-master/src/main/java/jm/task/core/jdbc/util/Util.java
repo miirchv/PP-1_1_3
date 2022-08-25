64884cffbc9b206207a22ce37b20ab2c7e941570
miirchv <98304456+miirchv@users.noreply.github.com>
2022-08-25 13:22:03 +0400

    Add files via upload

---
+++ PP-1_1_3-main/PP-1_1_3-main/PP_1_1_3-4_JDBC_Hibernate-master (1)/PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/my_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+    private Connection connection;
+
+    public Connection getConnection() {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Ошибка при создании соединения");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
